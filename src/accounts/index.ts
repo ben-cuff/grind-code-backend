69c4dda3410585de0f3bcf7c8f4852dbc7f4eeeb
ben-cuff <bencuff14@gmail.com>
2025-02-20 15:57:29 -0500

    feat: add user retrieval and deletion endpoints; improve error handling

--- src/accounts/index.ts
+++ src/accounts/index.ts
@@ -1,9 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
-const prisma = new PrismaClient();
+const prisma: PrismaClient = new PrismaClient();
 
 const router = express.Router();
+
+router.get("/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const user = await prisma.account.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ error: "A user with that userId was not found" });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ error: "A unexpected server error occurred: " + error });
+    }
+});
 
 router.post("/", async (req, res) => {
     try {
@@ -14,18 +36,51 @@ router.post("/", async (req, res) => {
                 .status(400)
                 .json({ error: "userId and email are required" });
         }
+        let newAccount;
+        try {
+            newAccount = await prisma.account.create({
+                data: {
+                    id: userId,
+                    email,
+                },
+            });
+        } catch {
+            return res
+                .status(400)
+                .json({ error: "Account with email or already exists" });
+        }
 
-        const newAccount = await prisma.account.create({
-            data: {
-                id: userId,
-                email,
-            },
-        });
         return res.status(201).json(newAccount);
     } catch (error) {
         return res
             .status(500)
-            .json({ error: "A server error occurred: " + error });
+            .json({ error: "A unexpected server error occurred: " + error });
+    }
+});
+
+router.delete("/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        try {
+            const user = await prisma.account.delete({
+                where: {
+                    id: userId,
+                },
+            });
+        } catch {
+            return res
+                .status(400)
+                .json({ error: "That account does not exist" });
+        }
+
+        return res
+            .status(200)
+            .json({ message: "Account successfully deleted" });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ error: "A unexpected server error occurred: " + error });
     }
 });
 
