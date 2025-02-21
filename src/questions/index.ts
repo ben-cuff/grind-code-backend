6ee853a8d2c87bf64ba3090be5327693e1c3027d
ben-cuff <bencuff14@gmail.com>
2025-02-21 11:06:42 -0500

    feat: add endpoint to retrieve a random question

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -8,6 +8,12 @@ router.get("/", async (_req, res) => {
     try {
         const questions = await prisma.question.findMany();
 
+        if (!questions) {
+            res.status(404).json({
+                error: "No questions found",
+            });
+        }
+
         res.status(200).json(questions);
     } catch (error) {
         res.status(500).json({
@@ -51,6 +57,27 @@ router.get("/:id", async (req, res) => {
         }
 
         res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({
+            error: "An unexpect server occurred: " + error,
+        });
+    }
+});
+
+router.get("/random-question", async (req, res) => {
+    try {
+        const questions = await prisma.question.findMany();
+
+        if (!questions) {
+            res.status(404).json({
+                error: "No questions found",
+            });
+        }
+
+        const randomIndex = Math.floor(Math.random() * questions.length);
+        const randomQuestion = questions[randomIndex];
+
+        res.status(200).json(randomQuestion);
     } catch (error) {
         res.status(500).json({
             error: "An unexpect server occurred: " + error,
