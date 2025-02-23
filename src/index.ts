5cc4c9dcf356bc37a4e113cf43812271a7d323cc
Ben Cuff <bencuff14@gmail.com>
2025-02-22 23:48:48 -0500

    feat: add @clerk/clerk-sdk-node dependency and integrate Clerk authentication in OpenAI route

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (_req, res) => {
     res.send("Hello from the API!");
 });
--- src/openai/index.ts
+++ src/openai/index.ts
@@ -1,44 +1,48 @@
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import { Request, Response, Router } from "express";
 import OpenAI from "openai";
-import { apiKeyMiddleware } from "..";
 
 const router = Router();
 
-router.post("/ask-ai", async (req: Request, res: Response) => {
-    try {
-        if (!apiKeyMiddleware(req, res)) return;
+router.post(
+    "/ask-ai",
+    ClerkExpressRequireAuth(),
+    async (req: Request, res: Response) => {
+        try {
+            const { message } = req.body;
 
-        const { message } = req.body;
+            if (!message) {
+                res.status(400).json({
+                    error: "Invalid or missing 'message' field",
+                });
+            }
 
-        if (!message) {
-            res.status(400).json({
-                error: "Invalid or missing 'message' field",
+            const openai = new OpenAI({
+                apiKey: process.env.OPENAI_API_KEY,
+            });
+
+            const response = await openai.chat.completions.create({
+                model: "gpt-4o-mini",
+                messages: [
+                    {
+                        content: message,
+                        role: "assistant",
+                    },
+                ],
+            });
+
+            res.status(200).json({
+                message: response.choices[0]?.message.content,
+            });
+        } catch (error) {
+            console.error("Error:", error);
+            res.status(500).json({
+                error: "Failed to generate response, it is likely that your API key is broken or out of credit",
+                errorMessage: error,
             });
         }
-
-        const openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
-
-        const response = await openai.chat.completions.create({
-            model: "o3-mini",
-            messages: [
-                {
-                    content: message,
-                    role: "assistant",
-                },
-            ],
-        });
-
-        res.status(200).json({ message: response.choices[0]?.message.content });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({
-            error: "Failed to generate response, it is likely that your API key is broken or out of credit",
-            errorMessage: error,
-        });
     }
-});
+);
 
 export interface Message {
     id: string;
